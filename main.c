6bd97b8a1e40cf674be467509f3a27437e58eb78
ysoroko <ysoroko@student.s19.be>
2021-08-16 16:38:00 +0200

    fixed non numerical input error detection

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 11:04:33 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/08/16 14:01:04 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/08/16 16:36:43 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,16 @@ static int	ft_main_args_error(int argc, char **argv)
 	if (argc > 6 || argc < 5)
 		return (ft_puterr("Wrong number of arguments"));
 	i = 0;
-	j = -1;
 	while (++i < argc)
 	{
-		if (!ft_strchrset(argv[i], BASE_TEN))
-			return (ft_puterr("Found non numerical input"));
+		j = -1;
+		while (argv[i][++j])
+		{
+			if (!ft_strchr(BASE_TEN, argv[i][j]))
+				return (ft_puterr("Found non numerical input"));
+		}
 		if (ft_atol(argv[i]) <= 0 || ft_atol(argv[i]) > INT_MAX)
-			return (ft_puterr("Only integer values are accepted"));
+			return (ft_puterr("Outside integers in range [1 ; 2147483647]"));
 	}
 	return (0);
 }
--- utils/ft_strings_utils.c
+++ utils/ft_strings_utils.c
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 11:43:38 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/08/16 13:54:16 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/08/16 16:37:24 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,6 @@ char	*ft_strchr(char *str, int c)
 	if (str[i] == c)
 	{
 		return (&str[i]);
-	}
-	return (0);
-}
-
-char	*ft_strchrset(char *str, char *charset)
-{
-	int		i;
-	char	*found;
-
-	if (!str || !charset)
-		return (0);
-	i = -1;
-	while (str[++i])
-	{
-		found = ft_strchr(charset, str[i]);
-		if (found)
-			return (found);
 	}
 	return (0);
 }
